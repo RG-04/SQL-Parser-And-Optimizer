d17a10d36b1e65f0505d197cda126a7dd37d4f96
Harshit Jain <harshitjain0502@gmail.com>
2025-04-15 14:26:03 +0530

    test query

--- final_parser/test_subquery.sql
+++ final_parser/test_subquery.sql
@@ -5,9 +5,9 @@ JOIN (SELECT b.id, b.join_key_ab FROM table_b b WHERE b.value = 'common_value_1'
 JOIN (SELECT b.id, b.join_key_ab FROM table_b b WHERE b.value = 'common_value_1') tmp2
   ON a.join_key_ab = tmp2.join_key_ab
 JOIN (SELECT b.id, b.join_key_ab FROM table_b b WHERE b.value = 'common_value_1') tmp3
-  ON a.join_key_ab = tmp2.join_key_ab
+  ON a.join_key_ab = tmp3.join_key_ab
 JOIN (SELECT b.id, b.join_key_ab FROM table_b b WHERE b.value = 'common_value_1') tmp4
-  ON a.join_key_ab = tmp2.join_key_ab
+  ON a.join_key_ab = tmp4.join_key_ab
 JOIN table_c c
   ON a.join_key_ac = c.join_key_ac
 WHERE a.id > 100;
